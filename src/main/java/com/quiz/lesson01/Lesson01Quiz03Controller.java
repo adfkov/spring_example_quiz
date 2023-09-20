3cd9fea8c64be0b30b57a48225a21485f718e017
학원 <test@test.com>
2023-09-20 21:45:42 +0900

    0920

--- src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Lesson01Quiz03Controller {
 	@RequestMapping("/lesson01/quiz03/1")
 	public String quiz03() {
+		// @ResponseBody 어노테이션이 없을 때 String을 리턴하면
+		// ViewResolver로 동작
 		return "lesson01/quiz03";
 	}
 }
